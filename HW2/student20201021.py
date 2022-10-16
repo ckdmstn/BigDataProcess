812f25281b4bc78542a50669d8e97b03589e8d67
ckdmstn <eunsu321@naver.com>
2022-10-16 22:26:53 +0900

    modify student20201021.py

--- HW2/student20201021.py
+++ HW2/student20201021.py
@@ -19,29 +19,35 @@ for row in ws:
 		number += 1
 	row_id += 1
 
+dict = {string : 0 for string in score}
 score.sort()
 score.reverse()
 grade = [number*0.15, number*0.3, number*0.5, number*0.7, number*0.85]
 
 count = 1;
-for s in range(len(score)):
-	if s+1 == len(score) or score[s] != score[s+1]:
-		row_id = 1;
-		for row in ws:
-			if ws.cell(row = row_id, column = 7).value == score[s]:
-				if count <= grade[0]:
-					ws.cell(row = row_id, column = 8).value = "A+"
-				elif count <= grade[1]:
-					ws.cell(row = row_id, column = 8).value = "A0"
-				elif count <= grade[2]:
-					ws.cell(row = row_id, column = 8).value = "B+"
-				elif count <= grade[3]:
-					ws.cell(row = row_id, column = 8).value = "B0"
-				elif count <= grade[4]:
-					ws.cell(row = row_id, column = 8).value = "C+"
-				else:
-					ws.cell(row = row_id, column = 8).value = "C0"
-			row_id += 1
+for i in range(number):
+	if i+1 != number:
+		if score[i] != score[i+1]:
+			dict[score[i]] = count
+	elif i+1 == number:
+		dict[score[i]] = count
 	count += 1
+
+row_id = 1
+for row in ws:
+	if row_id != 1:
+		if dict[ws.cell(row = row_id, column = 7).value] <= grade[0]:
+			ws.cell(row = row_id, column = 8).value = "A+"
+		elif dict[ws.cell(row = row_id, column = 7).value] <= grade[1]:
+			ws.cell(row = row_id, column = 8).value = "A0"
+		elif dict[ws.cell(row = row_id, column = 7).value] <= grade[2]:
+			ws.cell(row = row_id, column = 8).value = "B+"
+		elif dict[ws.cell(row = row_id, column = 7).value] <= grade[3]:
+			ws.cell(row = row_id, column = 8).value = "B0"
+		elif dict[ws.cell(row = row_id, column = 7).value] <= grade[4]:
+			ws.cell(row = row_id, column = 8).value = "C+"
+		else:
+			ws.cell(row = row_id, column = 8).value = "C0"
+	row_id += 1
 
 wb.save( "student.xlsx" )
