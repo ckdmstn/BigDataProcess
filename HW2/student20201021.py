5faaa57507808a6ee145d0f0cedc125d590a9a8d
ckdmstn <eunsu321@naver.com>
2022-10-17 18:44:16 +0900

    modify4 st20201021

--- HW2/student20201021.py
+++ HW2/student20201021.py
@@ -28,24 +28,36 @@ score_list = list(dictionary.keys())
 grade = [int(number*0.15), int(number*0.3), int(number*0.5), int(number*0.7), int(number*0.85)]
 
 count = 0
+grade_dic = { 'A' : 0, 'B' : 0, 'C' : 0 }
 for i in range(len(score_list)):
 	row_id = 1
 	score = float(score_list[i])
 	count += int(dictionary[score_list[i]])
+	stGrade = ""
+	if count <= grade[1]:
+		if count <= grade[0]:
+			stGrade = "A+"
+		else:
+			stGrade = "A0"
+		grade_dic['A'] += int(dictionary[score_list[i]])
+	elif count <= grade[3]:
+		n = grade[3] - grade_dic['A']
+		if int(dictionary[score_list[i]]) <= n / 2 - grade_dic['B']:
+			stGrade = "B+"
+		else:
+			stGrade = "B0"
+		grade_dic['B'] += int(dictionary[score_list[i]])
+	else:
+		n = number - grade_dic['A'] - grade_dic['B']
+		if int(dictionary[score_list[i]]) <= n / 2 - grade_dic['C']:
+			stGrade = "C+"
+		else:
+			stGrade = "C0"
+		grade_dic['C'] += int(dictionary[score_list[i]])
+
 	for row in ws:
 		if ws.cell(row = row_id, column = 7).value == score:
-			if count <= grade[0]:
-				ws.cell(row = row_id, column = 8).value = "A+"
-			elif count <= grade[1]:
-				ws.cell(row = row_id, column = 8).value = "A0"
-			elif count <= grade[2]:
-				ws.cell(row = row_id, column = 8).value = "B+"
-			elif count <= grade[3]:
-				ws.cell(row = row_id, column = 8).value = "B0"
-			elif count <= grade[4]:
-				ws.cell(row = row_id, column = 8).value = "C+"
-			else:
-				ws.cell(row = row_id, column = 8).value = "C0"
+			ws.cell(row = row_id, column = 8).value = stGrade
 		row_id += 1
 
 wb.save( "student.xlsx" )
