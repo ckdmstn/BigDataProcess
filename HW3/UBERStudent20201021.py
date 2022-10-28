d4eedf85ade2f23832b368e9786a231469afd009
ckdmstn <eunsu321@naver.com>
2022-10-29 02:29:13 +0900

    modify2

--- HW3/UBERStudent20201021.py
+++ HW3/UBERStudent20201021.py
@@ -11,13 +11,25 @@ with open(filename, "rt") as fp:
 	for line in fp:
 		list = line.split(",")
 		date = list[1].split("/")
+
 		year = int(date[2])
 		month = int(date[0])
 		day = int(date[1])
 		dayOfW = calendar.weekday(year, month, day)
 		list3 = list[3][0:-1]
 		str = list[0] + "," + dayofweek[dayOfW] + " " + list[2] + "," + list3
+
+		index = len(strList)
+		for i, s in enumerate(reversed(strList)):
+			if s.startswith(list[0]):
+				index = index-i
+				break;
 		strList.append(str)
+		i = len(strList)-2
+		while i >= index:
+			strList[i+1] = strList[i]
+			i -= 1
+		strList[index] = str
 
 with open(outputFilename, "wt") as fp:
 	for line in strList:
