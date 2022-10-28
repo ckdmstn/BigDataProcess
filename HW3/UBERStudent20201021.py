3bc8acb46a0040cae883ae0fea62567d473de6a2
ckdmstn <eunsu321@naver.com>
2022-10-29 00:19:54 +0900

    add UBER

---
+++ HW3/UBERStudent20201021.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import calendar
+
+filename = str(sys.argv[1])
+outputFilename = str(sys.argv[2])
+
+dayofweek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+strList = []
+with open(filename, "rt") as fp:
+	for line in fp:
+		list = line.split(",")
+		date = list[1].split("/")
+		year = int(date[2])
+		month = int(date[0])
+		day = int(date[1])
+		dayOfW = calendar.weekday(year, month, day)
+		list3 = list[3][0:-1]
+		str = list[0] + "," + dayofweek[dayOfW] + " " + list[2] + "," + list3
+		strList.append(str)
+
+with open(outputFilename, "wt") as fp:
+	if strList:
+		fp.write(strList[0])
+	for line in strList:
+		if (line != strList[0]):
+			fp.write("\n"+line)
