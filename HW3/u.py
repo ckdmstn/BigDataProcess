309908d97150b37f7f9628d285b51425a6e18041
ckdmstn <eunsu321@naver.com>
2022-10-29 02:57:25 +0900

    modify3

--- HW3/u.py
+++ HW3/u.py
@@ -17,31 +17,20 @@ with open(filename, "rt") as fp:
 		day = int(date[1])
 		dayOfW = calendar.weekday(year, month, day)
 		list3 = list[3][0:-1]
-		uList = [list[0], dayofweek[dayOfW], int(list[2]), int(list3)]
+		str = list[0] + "," + dayofweek[dayOfW] + " " + list[2] + "," + list3
 
 		index = len(strList)
 		for i, s in enumerate(reversed(strList)):
-			if s[0] == list[0]:
+			if s.startswith(list[0]):
 				index = index-i
 				break;
-		strList.append(uList)
+		strList.append(str)
 		i = len(strList)-2
 		while i >= index:
 			strList[i+1] = strList[i]
 			i -= 1
-		strList[index] = uList 
-
-uberList = []
-for li in strList:
-	isExist = False
-	for ul in uberList:
-		if ul[0] == li[0] and ul[1] == li[1]:
-			ul[2] += li[2]
-			ul[3] += li[3]
-			isExist = True
-	if isExist == False:
-		uberList.append(li)
+		strList[index] = str
 
 with open(outputFilename, "wt") as fp:
-	for i in range(len(uberList)):
-		fp.write("%s,%s %d,%d\n" % (uberList[i][0], uberList[i][1], uberList[i][2], uberList[i][3]))
+	for line in strList:
+		fp.write(line+"\n")
